ec7b739eb520741651ccaa841b0479e38d09c5e8
Robert Metzger <metzgerr@web.de>
2013-12-14 11:53:31 +0100

    maven nature

--- de/robertmetzger/protobuf/Launcher.java
+++
@@ -1,7 +0,0 @@
-package robertmetzger.protobuf;
-
-public class Launcher {
-	public static void main(String[] args) {
-
-	}
-}
---
+++ src/main/de/robertmetzger/protobuf/Launcher.java
@@ -0,0 +1,9 @@
+package de.robertmetzger.protobuf;
+
+import de.robertmetzger.protobuf.generated.Map;
+
+public class Launcher {
+	public static void main(String[] args) {
+		person = Map.newBuilder();
+	}
+}
