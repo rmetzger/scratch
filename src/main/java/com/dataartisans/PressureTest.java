04d4f824691349d90919dd6612006be1391b8180
Robert Metzger <rmetzger@apache.org>
2015-05-04 14:57:11 +0200

    new test

---
+++ src/main/java/com/dataartisans/PressureTest.java
@@ -0,0 +1,96 @@
+package com.dataartisans;
+
+import org.apache.flink.api.common.functions.RichFilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.function.source.RichParallelSourceFunction;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by robert on 5/4/15.
+ */
+public class PressureTest {
+
+	private static final Logger LOG = LoggerFactory.getLogger(PressureTest.class);
+
+	public static class Workload {
+		private byte[] data;
+		private int partition;
+		private long element;
+
+		public Workload(byte[] data, int partition, long element) {
+			this.data = data;
+			this.partition = partition;
+			this.element = element;
+		}
+
+		public byte[] getData() {
+			return data;
+		}
+
+		public void setData(byte[] data) {
+			this.data = data;
+		}
+
+		public int getPartition() {
+			return partition;
+		}
+
+		public void setPartition(int partition) {
+			this.partition = partition;
+		}
+
+		public long getElement() {
+			return element;
+		}
+
+		public void setElement(long element) {
+			this.element = element;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+		final int mb = Integer.valueOf(args[0]);
+		final int dataSize = 1024 * 1024 * mb; // MB
+		final int sourcePar = Integer.valueOf(args[1]);
+		final int sinkPar = Integer.valueOf(args[2]);
+		final int logFreq = Integer.valueOf(args[3]);
+		DataStream<Workload> data = see.addSource(new RichParallelSourceFunction<Workload>() {
+
+			@Override
+			public void run(Collector<Workload> collector) throws Exception {
+				long element = 0;
+				final int partition = getRuntimeContext().getIndexOfThisSubtask();
+				final byte[] data = new byte[dataSize];
+				while (true) {
+					if(element % logFreq == 0) {
+						LOG.info("Elements processed {}, data send {} GB", element, ((dataSize*element)/(1024*1024*1024)) );
+					}
+					collector.collect(new Workload(data, partition, element++));
+				}
+			}
+
+			@Override
+			public void cancel() {
+
+			}
+		}).setParallelism(sourcePar);
+
+		DataStream<Workload> out = data.broadcast().filter(new RichFilterFunction<Workload>() {
+			long elements = 0;
+			@Override
+			public boolean filter(Workload value) throws Exception {
+				elements++;
+				if(elements % logFreq == 0) {
+					LOG.info("Elements received {}, data received {} GB", elements, ((dataSize*elements)/(1024*1024*1024)) );
+				}
+				return false;
+			}
+		}).setParallelism(sinkPar);
+
+		see.execute("Data flusher");
+	}
+}
