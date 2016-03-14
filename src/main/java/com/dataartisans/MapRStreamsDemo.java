24469ce49c8d5a40853300b9f3f0645542576f42
Robert Metzger <rmetzger@apache.org>
2016-03-14 14:12:02 +0100

    Create demo file

---
+++ src/main/java/com/dataartisans/MapRStreamsDemo.java
@@ -0,0 +1,59 @@
+package com.dataartisans;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+/**
+ * Demo for reading from a MapR stream
+ */
+public class MapRStreamsDemo {
+
+	public static void main(String[] args) throws Exception {
+		// parse input arguments
+		final ParameterTool parameterTool = ParameterTool.fromArgs(args);
+
+		if(parameterTool.getNumberOfParameters() < 3) {
+			System.out.println("Missing parameters!\nUsage: Kafka --topic <topic> --bootstrap.servers <kafka brokers> --group.id <some id>");
+			System.exit(1);
+		}
+
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.getConfig().disableSysoutLogging();
+		env.getConfig().setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 10000));
+		env.enableCheckpointing(5000); // create a checkpoint every 5 secodns
+		env.getConfig().setGlobalJobParameters(parameterTool); // make parameters available in the web interface
+
+		DataStream<String> messageStream = env
+				.addSource(new FlinkKafkaConsumer09<>(
+						parameterTool.getRequired("topic"),
+						new SimpleStringSchema(),
+						parameterTool.getProperties()));
+
+		// write kafka stream to standard out.
+		messageStream.print();
+
+		env.execute("Read from MapR Streams example");
+	}
+}
